65b26db73509784096080e54771b5d107b4aec23
CamiloCuenca <camilocuenca1810@gmail.com>
2024-11-10 21:52:14 -0500

    feat(productos): separar las cards de productos como componente PHP y cargar datos desde la base de datos
    Se creó un componente PHP separado para las cards de productos que carga datos directamente de la base de datos. Además, en productos.php, las cards ahora se generan de manera dinámica según la cantidad de productos disponibles en la base de datos.

--- productos.php
+++ productos.php
@@ -23,48 +23,9 @@
         <p>¡Lo que necesitas está aquí! Productos seleccionados con cuidado.</p>
     </section>
     
-
-    <div class="container">
-        <h2 class="text-center">Productos Destacados</h2>
-        <div class="product-container">
-            <div class="card product">
-                <img src="images/medicamento1.jpg" alt="Producto Medicamento 1">
-                <h5>Multivitamínico</h5>
-                <p>$20,000</p>
-                <button>Comprar</button>
-            </div>
-            <div class="card product">
-                <img src="images/medicamento2.jpg" alt="Producto Medicamento 2">
-                <h5>Antibiótico</h5>
-                <p>$15,000</p>
-                <button>Comprar</button>
-            </div>
-            <div class="card product">
-                <img src="images/cuidado1.jpg" alt="Producto Cuidado Personal 1">
-                <h5>Crema Hidratante</h5>
-                <p>$25,000</p>
-                <button>Comprar</button>
-            </div>
-            <div class="card product">
-                <img src="images/cuidado2.jpg" alt="Producto Cuidado Personal 2">
-                <h5>Shampoo Suave</h5>
-                <p>$18,000</p>
-                <button>Comprar</button>
-            </div>
-            <div class="card product">
-                <img src="images/cosmetico1.jpg" alt="Producto Cosmético 1">
-                <h5>Base de Maquillaje</h5>
-                <p>$30,000</p>
-                <button>Comprar</button>
-            </div>
-            <div class="card product">
-                <img src="images/cosmetico2.jpg" alt="Producto Cosmético 2">
-                <h5>Labial Hidratante</h5>
-                <p>$12,000</p>
-                <button>Comprar</button>
-            </div>
-        </div>
-    </div>
+    <?php
+    include 'componentes\card.php';
+    ?>
 
     <?php
     include 'componentes\footer.php';
